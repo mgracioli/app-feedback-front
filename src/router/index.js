fdf083b8d6cc101697b13995649bfa64039d3e54
mgracioli <michel-rg@hotmail.com>
2023-02-26 15:07:44 -0300

    Criação das prmeiras rotas e configurações adicionais

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+const Home = () => import('../views/Home')
+const Feedbacks = () => import('../views/Feedbacks')
+const Credentials = () => import('../views/Credentials')
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/feedbacks',
+    name: 'Feedbacks',
+    component: Feedbacks,
+    meta: {
+      hasAuth: true // para acessar essa rota, é necesário estar autenticado
+    }
+  },
+  {
+    path: '/credentials',
+    name: 'Credentials',
+    component: Credentials,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*', // regex do Vue Router (caso não encontre a rota nas rotas definidas acima, redireciona para a Home)
+    redirect: 'Home'
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory('/'),
   routes
 })
 
---
+++ src/views/Feedbacks/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Aqui é o Feedback</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
